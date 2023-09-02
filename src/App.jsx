1d907818306c3ce81816f34906cf62ad8c9f60d2
DivyamChauhan <mi.divyam@gmail.com>
2023-09-02 16:45:23 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
 
   useEffect(() => {
     apiTesting();
@@ -17,7 +18,12 @@ function App() {
       dispatch(getApiConfiguration(res));
     });
   };
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
